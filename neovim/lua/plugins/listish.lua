2cdcf5ce10c0898cfacd4e2011b6a24abd0b3176
konnlori <kastentop2005@gmail.com>
2023-06-25 22:22:08 +0300

    neovim: install missing dependencies

---
+++ neovim/lua/plugins/listish.lua
@@ -0,0 +1,10 @@
+return {
+  "arsham/listish.nvim",
+  dependencies = {
+    "arsham/arshlib.nvim",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  config = true,
+  -- or to provide configuration
+  -- config = { theme_list = false, ..}
+}
